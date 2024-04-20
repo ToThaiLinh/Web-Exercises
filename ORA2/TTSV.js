c1bc6fb21cfd21bdf1bc9c37dfc88af564808427
Linh <tothailinhytbg2003@gmail.com>
2024-04-20 14:24:27 +0700

    ORA2

--- ORA2/TTSV.js
+++ ORA2/TTSV.js
@@ -50,67 +50,69 @@ var srcImage_new;
 function chooseFile(fileInput) {
     if(fileInput.files && fileInput.files[0]) {
         var reader = new FileReader();
-        var imagePre = document.getElementById("ctl00_ctl00_contentPane_MainPanel_MainContent_UserImageCPanel_imgUserImage");
+        var imagePreview = document.getElementById("ctl00_ctl00_contentPane_MainPanel_MainContent_UserImageCPanel_imgUserImage");
 
         reader.onload = function(e) {
-            imagePre.src = e.target.result;
+            imagePreview.src = e.target.result;
             srcImage_new = e.target.result;
         }
         reader.readAsDataURL(fileInput.files[0]);
     }
 }
 
-document.getElementById("myImage").addEventListener('change', function() {
+document.getElementById("fileInput").addEventListener('change', function() {
     chooseFile(this)});
 
 
 function editInfor() {
     addButton.style.display = 'block';
-    myImage.style.display = 'block';
+    fileInput.style.display = 'block';
 
     document.getElementById("HoTen").innerHTML = `<input type="text" id="editName" name="editName" value="${TTSV_new.studentHoTen}" />`;
     document.getElementById("NamVaoTruong").innerHTML = `<input type="text" id="editNamVaoTruong" name="editNamVaoTruong" value="${TTSV_new.studentNamVaoTruong}" />`;
     document.getElementById("BacDaoTao").innerHTML = 
-    '<select id="editBacDaoTao" name="editBacDaoTao">' + 
-    '<option value="Đào tạo đại trà">Đào tạo đại trà</option>' +
-    '<option value="Chương trình tiên tiến">Chương trình tiên tiến</option>' + 
-    '<option value="Chương trình tài năng">Chương trình tài năng</option>' +
-    '<option value="Khác">Khác</option>' +
-    '</select>';
+    `<select id="editBacDaoTao" name="editBacDaoTao">
+    <option value="Đại học đại trà">Đại học đại trà</option>
+    <option value="Chương trình tiên tiến">Chương trình tiên tiến</option>
+    <option value="Chương trình tài năng">Chương trình tài năng</option>
+    <option value="Khác">Khác</option>' +
+    </select>`;
+    document.querySelector(`option[value="${TTSV_new.studentBacDaoTao}"]`).selected = true;
     document.getElementById("ChuongTrinh").innerHTML = `<input type="text" id="editChuongTrinh" name="editChuongTrinh" value="${TTSV_new.studentChuongTrinh}" />`;
     document.getElementById("Khoa").innerHTML = 
-    '<select id="editKhoa">' + 
-    '<option value="Trường Công nghệ Thông tin và Truyền Thông">Trường Công nghệ Thông tin và Truyền thông</option>' + 
-    '<option value="Trường Cơ khí" >Trường Cơ khí</option>' + 
-    '<option value="Trường Điện - Điện tử" >Trường Điện - Điện tử</option>' +
-    '<option value="Trường Hóa và Khoa học sự sống">Trường Hoá và Khoa học sự sống</option>' +
-    '<option value="Trường Vật Liệu">Trường Vật liệu</option>' +
-    '<option value="Khoa Toán - Tin" >Khoa Toán - Tin</option>' +
-    '<option value="Khoa vật lý Kỹ thuật" >Khoa Vật lý Kỹ thuật</option>' +
-    '<option value="Viện kinh tế và Quản lý" >Viện Kinh tế và Quản lý</option>' +
-    '<option value="Khoa Ngoại ngữ" >Khoa Ngoại ngữ</option>' +
-    '<option value="Khoa Khoa học và Công nghệ Giáo dục" >Khoa Khoa học và Công nghệ Giáo dục</option>' +
-    '</select>';
+    `<select id="editKhoa">
+    <option value="Trường Công nghệ thông tin và Truyền thông">Trường Công nghệ thông tin và Truyền thông</option>
+    <option value="Trường Cơ khí" >Trường Cơ khí</option>
+    <option value="Trường Điện - Điện tử" >Trường Điện - Điện tử</option>
+    <option value="Trường Hóa và Khoa học sự sống">Trường Hoá và Khoa học sự sống</option>
+    <option value="Trường Vật Liệu">Trường Vật liệu</option>
+    <option value="Khoa Toán - Tin" >Khoa Toán - Tin</option>
+    <option value="Khoa vật lý Kỹ thuật" >Khoa Vật lý Kỹ thuật</option>
+    <option value="Viện kinh tế và Quản lý" >Viện Kinh tế và Quản lý</option>
+    <option value="Khoa Ngoại ngữ" >Khoa Ngoại ngữ</option>
+    <option value="Khoa Khoa học và Công nghệ Giáo dục" >Khoa Khoa học và Công nghệ Giáo dục</option>
+    </select>`;
+    document.querySelector(`option[value="${TTSV_new.studentKhoa}"]`).selected = true;
     document.getElementById("TinhTrangHocTap").innerHTML = 
     `<select id="editTinhTrangHocTap" name="editTinhTrangHocTap">
     <option value="Học">Học</option>
     <option value="Thôi học">Thôi học</option>
     <option value="Đã tốt nghiệp">Đã tốt nghiệp</option>
     </select>`;
+    document.querySelector(`option[value="${TTSV_new.studentTinhTrangHocTap}"]`).selected = true;
     document.getElementById("GioiTinh").innerHTML = 
     `<form style="font-weight: normal !important;">
-    <input type="radio" id="nam" name="gioi_tinh" value="Nam" checked>
-    <label for="nam">Name</label>
+    <input type="radio" id="nam" name="gioi_tinh" value="Nam">
+    <label for="nam" style="margin-right: 10px;">Name</label>
     <input type="radio" id="nu" name="gioi_tinh" value="Nữ">
-    <label for="nu">Nữ</label>
+    <label for="nu" style="margin-right: 10px;">Nữ</label>
     <input type="radio" id="khac" name="gioi_tinh" value="Khác">
-    <label for="khac">Khác</label>
+    <label for="khac" style="margin: 10px;">Khác</label>
     </form>`
+    document.querySelector(`input[value="${TTSV_new.studentGioiTinh}"]`).checked = true;
     document.getElementById("Lop").innerHTML = `<input type="text" id="editLop" name="editLop" value="${TTSV_new.studentLop}" />`;
     document.getElementById("KhoaHoc").innerHTML = `<input type="text" id="editKhoaHoc" name="editKhoaHoc" value="${TTSV_new.studentKhoaHoc}" />`;
     document.getElementById("Email").innerHTML = `<input type="text" id="editEmail" name="editEmail" value="${TTSV_new.studentEmail}" />`;
-
-
 }
 
 function functionOK() {
@@ -125,11 +127,10 @@ function functionOK() {
     TTSV_new.studentEmail = document.getElementById("editEmail").value;
     TTSV_new.srcImage = srcImage_new;
 
-    let selecter = document.querySelector("input[name='gioi_tinh']:checked").value;
-    TTSV_new.studentGioiTinh = selecter;
+    TTSV_new.studentGioiTinh = document.querySelector("input[name='gioi_tinh']:checked").value;
 
     addButton.style.display = 'none';
-    myImage.style.display = 'none';
+    fileInput.style.display = 'none';
     createBG();
     console.log(TTSV_new);
 }
@@ -152,14 +153,14 @@ function functionReset() {
     document.getElementById("ctl00_ctl00_contentPane_MainPanel_MainContent_UserImageCPanel_imgUserImage").src = TTSV_default["srcImage"];
 
     addButton.style.display = 'none';
-    myImage.style.display = 'none';
+    fileInput.style.display = 'none';
     console.log(TTSV_default);
 }
 
 function functionCancel() {
     createBG();
     addButton.style.display = 'none';
-    myImage.style.display = 'none';
+    fileInput.style.display = 'none';
     console.log(TTSV_new);
 }
 
